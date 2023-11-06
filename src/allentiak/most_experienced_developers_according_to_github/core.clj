185cc30486167d8dce4d2d3d9e4f5ca4362c72fe
Leandro Doctors <ldoctors@gmail.com>
2023-11-06 22:08:43 +0100

    Learnt how to properly read JSONs in Clojure

--- src/allentiak/most_experienced_developers_according_to_github/core.clj
+++ src/allentiak/most_experienced_developers_according_to_github/core.clj
@@ -1,10 +1,52 @@
 (ns allentiak.most-experienced-developers-according-to-github.core
-  (:gen-class))
+  (:gen-class)
+  (:require
+   [malli.core :as m]))
 
 (defn foo []
-  1)
+  8)
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(def members-json-schema
+  (m/schema
+    [:vector
+     [:map
+      [:login
+       [:string {:min 1}]]]]))
+
+(comment
+  (require '[malli.experimental.describe :as med])
+  (require '[clojure.data.json :as json])
+  (require '[clojure.pprint :as pprint])
+
+  (med/describe [:map {:closed true}
+                 [:x {:optional true} int?]
+                 [:y :boolean]])
+
+  (med/describe [:vector
+                 [:map
+                  [:login [:string {:min 1}]]]])
+
+  (defn json-sample-file []
+    (json/read-str
+      (slurp "resources/members--codecentric--minimized.json")
+      :key-fn keyword))
+
+
+  (defn json-sample-file []
+    (json/read-str
+      (slurp "resources/members--minimized.json")
+      :key-fn keyword))
+  (json/read-str "[{\"login\": \"soitensa\"}, {\"login\":\"rtdud\"}]"
+                 :key-fn keyword)
+
+  (m/validate members-json-schema (json/read-str (slurp "resources/members--minimized.json")))
+  (m/explain members-json-schema (j))
+  (m/validate members-json-schema (json-sample-file))
+  (m/explain members-json-schema (json-sample-file))
+  ,
+  ,)
--- test/allentiak/most_experienced_developers_according_to_github/core_test.clj
+++ test/allentiak/most_experienced_developers_according_to_github/core_test.clj
@@ -1,9 +1,17 @@
 (ns allentiak.most-experienced-developers-according-to-github.core-test
   (:require
    [allentiak.most-experienced-developers-according-to-github.core :as sut]
+   [clj-http.client :as client]
    [clojure.test :refer [deftest testing]]
-   [expectations.clojure.test :refer [expect]]))
+   [expectations.clojure.test :refer [expect]]
+   [malli.core :as m]))
 
-(deftest smoke-test
-  (testing "this should fail"
-    (expect (= (sut/foo) 2))))
+(deftest github-rest-api
+  (testing "querying GitHub REST API..."
+    (let [response-body (:body (client/get "https://api.github.com/orgs/codecentric/members"))]
+      (expect (m/validate sut/members-json-schema response-body)))))
+
+(comment
+  (->
+    (client/get "https://api.github.com/orgs/codecentric/members" {:accept :json})
+    :body))
