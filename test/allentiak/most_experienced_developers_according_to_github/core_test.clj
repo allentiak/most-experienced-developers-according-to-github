489f3462959aa884ebc7c2428b4f0837b190ecd0
Leandro Doctors <ldoctors@gmail.com>
2023-11-07 20:05:50 +0100

    refactor: http client alias: client -> http

--- test/allentiak/most_experienced_developers_according_to_github/core_test.clj
+++ test/allentiak/most_experienced_developers_according_to_github/core_test.clj
@@ -1,17 +1,17 @@
 (ns allentiak.most-experienced-developers-according-to-github.core-test
   (:require
    [allentiak.most-experienced-developers-according-to-github.core :as sut]
-   [clj-http.client :as client]
+   [clj-http.client :as http]
    [clojure.test :refer [deftest testing]]
    [expectations.clojure.test :refer [expect]]
    [malli.core :as m]))
 
 (deftest github-rest-api
   (testing "querying GitHub REST API..."
-    (let [response-body (:body (client/get "https://api.github.com/orgs/codecentric/members"))]
+    (let [response-body (:body (http/get "https://api.github.com/orgs/codecentric/members"))]
       (expect (m/validate sut/members-json-schema response-body)))))
 
 (comment
   (->
-    (client/get "https://api.github.com/orgs/codecentric/members" {:accept :json})
+    (http/get "https://api.github.com/orgs/codecentric/members" {:accept :json})
     :body))
