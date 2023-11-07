ec8d756dbc448a8af15630d1bc4b4e10d5517ee6
Leandro Doctors <ldoctors@gmail.com>
2023-11-07 23:46:18 +0100

    rename: test name

--- test/allentiak/most_experienced_developers_according_to_github/core_test.clj
+++ test/allentiak/most_experienced_developers_according_to_github/core_test.clj
@@ -7,12 +7,12 @@
    [expectations.clojure.test :refer [expect]]
    [malli.core :as m]))
 
-(deftest github-members-rest-api
-  (testing "actual GitHub REST API JSON response..."
+(deftest github-orgs-members-rest-api-test
+  (testing "actual GitHub REST API JSON response"
     (let [response-body (:body (http/get sut/members-endpoint-url))
           json-response (json/read-str response-body :key-fn keyword)]
       (expect (m/validate sut/members-json-schema json-response))))
-  (testing "manually downloaded minimized JSON files..."
+  (testing "manually downloaded minimized JSON files"
     (let [mocked-json-response (json/read-str
                                 (slurp "resources/members--minimized.json")
                                 :key-fn keyword)]
