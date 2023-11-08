688275ab80bb5506c0b92f2adb41255696af037c
Leandro Doctors <ldoctors@gmail.com>
2023-11-08 22:30:48 +0100

    refactor: extract 'endpoints'

--- src/allentiak/most_experienced_developers_according_to_github/github_rest_client.clj
+++ src/allentiak/most_experienced_developers_according_to_github/github_rest_client.clj
@@ -1,28 +1,8 @@
-(ns allentiak.most-experienced-developers-according-to-github.github-rest-client
-  (:require
-   [malli.core :as m]
-   [allentiak.most-experienced-developers-according-to-github.github-rest-client.schemas :as schemas]))
-
-(def ^:const root-endpoint-url "https://api.github.com")
-
-(def ^:const org-name "codecentric")
-
-(defn get-members-endpoint-url [root-endpoint-url org-name]
-  (str root-endpoint-url "/orgs/" org-name "/members"))
-
-(def ^:const members-endpoint-url
-  (get-members-endpoint-url root-endpoint-url org-name))
-
-(defn get-user-endpoint-url [root-endpoint-url login]
-  (str root-endpoint-url "/users/" login))
-
-(defn get-repos-per-login-endpoint-url [root-endpoint-url login]
-  (str (get-user-endpoint-url root-endpoint-url login) "/repos"))
+(ns allentiak.most-experienced-developers-according-to-github.github-rest-client)
 
 (comment
   (require '[malli.experimental.describe :as med])
   (require '[clojure.data.json :as json])
-  (require '[clojure.pprint :as pprint])
   (require '[allentiak.most-experienced-developers-according-to-github.github-rest-client.schemas :as schemas])
   (med/describe [:map {:closed true}
                  [:x {:optional true} int?]
--- test/allentiak/most_experienced_developers_according_to_github/github_rest_client_test.clj
+++ test/allentiak/most_experienced_developers_according_to_github/github_rest_client_test.clj
@@ -1,6 +1,6 @@
 (ns allentiak.most-experienced-developers-according-to-github.github-rest-client-test
   (:require
-   [allentiak.most-experienced-developers-according-to-github.github-rest-client :as sut]
+   [allentiak.most-experienced-developers-according-to-github.github-rest-client.endpoints :as endpoints]
    [allentiak.most-experienced-developers-according-to-github.github-rest-client.schemas :as schemas]
    [clj-http.client :as http]
    [clojure.data.json :as json]
@@ -10,7 +10,7 @@
 
 (deftest org-members-fetch-test
   (testing "actual GitHub REST API JSON response"
-    (let [members-response-body-json (:body (http/get sut/members-endpoint-url))
+    (let [members-response-body-json (:body (http/get endpoints/members-url))
           members-response-body-map (json/read-str members-response-body-json :key-fn keyword)]
       (expect (m/validate schemas/members-response members-response-body-map))))
   (testing "manually downloaded minimized JSON files"
@@ -21,13 +21,13 @@
 
 (comment
   (->
-   (http/get (sut/members-endpoint-url {:accept :json}))
+   (http/get (endpoints/members-url {:accept :json}))
    :body))
 
 (deftest login-fetch-test
   (testing "actual GitHub REST API JSON response"
     (let [sample-login "allentiak"
-          sample-login-endpoint-url (sut/get-user-endpoint-url sut/root-endpoint-url sample-login)
+          sample-login-endpoint-url (endpoints/get-user-url endpoints/root-url sample-login)
           login-response-body-json (:body (http/get sample-login-endpoint-url))
           login-response-body-map (json/read-str login-response-body-json
                                                  :key-fn keyword)]
@@ -41,7 +41,7 @@
 (deftest repos-fetch-test
   (testing "actual GitHub REST API JSON response"
     (let [sample-login "allentiak"
-          repos-per-sample-login-endpoint-url (sut/get-repos-per-login-endpoint-url sut/root-endpoint-url sample-login)
+          repos-per-sample-login-endpoint-url (endpoints/get-repos-per-login-url endpoints/root-url sample-login)
           repos-response-body-json (:body (http/get repos-per-sample-login-endpoint-url))
           repos-response-doby-map (json/read-str repos-response-body-json
                                                  :key-fn keyword)]
