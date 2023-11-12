6a4cdf9e2395ad265ca0a05c5b21b69efdc03e7f
Leandro Doctors <ldoctors@gmail.com>
2023-11-12 05:13:58 +0100

    refactor: tests: data conversion

--- test/allentiak/most_experienced_developers_according_to_github/db_creation/data_conversion_test.clj
+++ test/allentiak/most_experienced_developers_according_to_github/db_creation/data_conversion_test.clj
@@ -8,6 +8,13 @@
    [malli.core :as m]))
 
 (deftest table-maps-creation-test
+  (testing "members table map creation"
+    (let [mocked-members-response-vector (json/read-str
+                                          (slurp "resources/members--minimized.json")
+                                          :key-fn keyword)
+          members-login-set (sut/generate-members-login-set mocked-members-response-vector)
+          generated-members-set (sut/generate-members-set members-login-set)]
+      (expect (m/validate schemas/members-set generated-members-set))))
   (testing "repos table map creation"
     (let [mocked-members-login-set #{"allentiak" "puredanger"}
           generated-repos-set (sut/generate-repos-set mocked-members-login-set)]
@@ -15,26 +22,18 @@
   (testing "languages table map creation"
     (let [mocked-repos-set (slurp "resources/sample-repos-set.edn")
           generated-languages-set (sut/generate-languages-set mocked-repos-set)]
-      (expect (m/validate schemas/languages-set generated-languages-set))))
-  (testing "members table map creation"
-    (let [mocked-members-response-set (set
-                                       (json/read-str
-                                        (slurp "resources/members--minimized.json")
-                                        :key-fn keyword))
-          members-login-set (sut/generate-members-login-set mocked-members-response-set)
-          generated-members-set (sut/generate-members-set members-login-set)]
-      (expect (m/validate schemas/members-set generated-members-set)))))
+      (expect (m/validate schemas/languages-set generated-languages-set)))))
 
 (comment
   (require '[allentiak.most-experienced-developers-according-to-github.db-creation.data-conversion :as sut]
            '[clojure.data.json :as json])
-  (def members-response-set (set (json/read-str
-                                  (slurp "resources/members--minimized.json")
-                                  :key-fn keyword)))
-  members-response-set
-  ;; => #{{:login "puredanger"} {:login "allentiak"}}
+  (def members-response-vector (json/read-str
+                                (slurp "resources/members--minimized.json")
+                                :key-fn keyword))
+  members-response-vector
+  ;; => [{:login "allentiak"} {:login "puredanger"}]
   (def members-login-set
-    (sut/generate-members-login-set members-response-set))
+    (sut/generate-members-login-set members-response-vector))
   members-login-set
   ;; => #{"allentiak" "puredanger"}
   (sut/generate-members-set members-login-set)
