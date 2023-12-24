046f5bda217b358caf26902937e8efffb67bc5b5
Leandro Doctors <ldoctors@gmail.com>
2023-12-24 20:03:54 +0100

    fixup! refactor+fix: REST schemas, DB schemas

--- src/allentiak/most_experienced_developers_according_to_github/db_creation/schemas.clj
+++ src/allentiak/most_experienced_developers_according_to_github/db_creation/schemas.clj
@@ -19,25 +19,35 @@
      member-map]))
 
 
+(def repos-map
+  (m/schema
+    [:map
+     {:closed true}
+     [:owner
+      [:string {min 1}]]
+     [:name
+      [:string {min 1}]]
+     [:main-language
+      [:maybe [:string {min 1}]]]]))
+
+
 (def repos-set
   (m/schema
     [:set
-     [:map
-      {:closed true}
-      [:owner
-       [:string {min 1}]]
-      [:name
-       [:string {min 1}]]
-      [:main-language
-       [:maybe [:string {min 1}]]]]]))
+     repos-map]))
+
+
+(def languages-map
+  (m/schema
+    [:map
+     {:closed true}
+     [:id
+      [:int]]
+     [:name
+      [:string {min 1}]]]))
 
 
 (def languages-set
   (m/schema
     [:set
-     [:map
-      {:closed true}
-      [:id
-       [:int]]
-      [:name
-       [:string {min 1}]]]]))
+     languages-map]))
