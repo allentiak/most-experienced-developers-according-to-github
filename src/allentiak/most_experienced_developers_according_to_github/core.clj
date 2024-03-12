43f0718a2b645ba3469d96273b13147e46a20434
Leandro Doctors <ldoctors@gmail.com>
2024-03-12 13:35:18 +0100

    feature: end-to-end members table data generation

--- src/allentiak/most_experienced_developers_according_to_github/core.clj
+++ src/allentiak/most_experienced_developers_according_to_github/core.clj
@@ -1,6 +1,16 @@
 (ns allentiak.most-experienced-developers-according-to-github.core
-  (:gen-class))
+  (:gen-class)
+  (:require
+   [allentiak.most-experienced-developers-according-to-github.data-fetching :as fetch]
+   [allentiak.most-experienced-developers-according-to-github.data-conversion :as convert]))
 
+(defn generate-members-table
+  [org-name]
+  (->
+    (fetch/org-members org-name)
+    (convert/members-response->user-logins-set)
+    (fetch/users-data)
+    (convert/user-login-responses-set->member-table-data-set)))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/allentiak/most_experienced_developers_according_to_github/core_test.clj
+++ test/allentiak/most_experienced_developers_according_to_github/core_test.clj
@@ -1,9 +1,74 @@
 (ns allentiak.most-experienced-developers-according-to-github.core-test
   (:require
     [allentiak.most-experienced-developers-according-to-github.core :as sut]
-    [expectations.clojure.test :refer [defexpect expect expecting]]))
+    [allentiak.most-experienced-developers-according-to-github.data-conversion.schemas :as schemas]
+    [clojure.edn :as edn]
+    [clojure.test :refer [use-fixtures]]
+    [expectations.clojure.test :refer [defexpect expect expecting]]
+    [malli.core :as m]))
 
 
-(defexpect core-should
-  (expecting "put 1 and 1 together :)"
-    (expect (= (+ 1 1) 2))))
+(defn with-defs
+  [test-fn]
+  (def test-resources-root-dir "resources/test/core/")
+  (def ^:const org-name "codecentric")
+  (test-fn)
+  (ns-unmap *ns* 'org-name)
+  (ns-unmap *ns* 'test-resources-root-dir))
+
+(use-fixtures :once with-defs)
+
+(defexpect generate-members-table-should
+  (expecting "generate members table from org-name"
+    (let [generated-members-table-data-set (sut/generate-members-table org-name)]
+      (expect (m/validate schemas/members-table-data generated-members-table-data-set)))))
+
+(comment
+  (m/validate schemas/members-table-data #{{:name "asv" :login "ass"}
+                                           {:login "suss" :name "ff"}})
+  ;; => true
+  (m/validate (m/schema
+                [:maybe
+                 [:string {:min 1}]]) nil)
+  ;; => true
+
+  (let [my-schema (m/schema
+                    [:set
+                     [:map
+                      {:closed true}
+                      [:login
+                       [:string {:min 1}]]
+                      [:name
+                       [:string]]]])]
+
+   (m/validate my-schema #{{:login "dickerpulli", :name "Thomas Bosch"}
+                           {:login "denniseffing", :name "Dennis Effing"}
+                           {:login "devshred", :name "Johannes Schmidt"}
+                           {:login "mniehoff", :name "Matthias Niehoff"}
+                           {:login "dvdgsng", :name nil}
+                           {:login "mkresse", :name "Martin Kresse"}
+                           {:login "danielkocot", :name "Daniel Kocot"}
+                           {:login "rfalke", :name nil}
+                           {:login "feltenj", :name nil}
+                           {:login "robaca", :name "Carsten Rohrbach"}
+                           {:login "renetalk", :name "René Bohrenfeldt"}
+                           {:login "s7nio", :name "Sebastian Steiner"}
+                           {:login "F43nd1r", :name nil}
+                           {:login "ljouon", :name "Lars Jouon"}
+                           {:login "rueckemann", :name "Lars Rückemann"}
+                           {:login "goekhanm", :name "Gökhan Makinist"}
+                           {:login "jonashackt", :name "Jonas Hecht"}
+                           {:login "lukas-reining", :name "Lukas Reining"}
+                           {:login "PSanetra", :name "Philip Sanetra"}
+                           {:login "maiksensi", :name "maiksensi"}
+                           {:login "erikpetzold", :name "Erik Petzold"}
+                           {:login "hw-cc", :name nil}
+                           {:login "rhoetsc", :name "Ronald Hötschl"}
+                           {:login "romansey", :name "Roman Seyffarth"}
+                           {:login "dirkheinkecc", :name "Dirk Heinke"}
+                           {:login "danielschleindlsperger", :name "Daniel Schleindlsperger"}
+                           {:login "danielbayerlein", :name "Daniel Bayerlein"}
+                           {:login "reitzig", :name "Raphael"}
+                           {:login "JonasGroeger", :name "Jonas Gröger"}
+                           {:login "romanigk", :name "Robert Manigk"}}))
+  ,)
