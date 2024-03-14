e66ba4dbafceeea7dacb7609bc8ae4a330d9c55d
Leandro Doctors <ldoctors@gmail.com>
2024-03-14 13:43:30 +0100

    database creation: clean up

--- src/allentiak/most_experienced_developers_according_to_github/database_creation.clj
+++ src/allentiak/most_experienced_developers_according_to_github/database_creation.clj
@@ -1,7 +1,6 @@
 (ns allentiak.most-experienced-developers-according-to-github.database-creation
   (:require
-   [next.jdbc :as jdbc]
-   [next.jdbc.sql :as sql]))
+   [next.jdbc :as jdbc]))
 
 (def db
   {:dbtype "h2"
@@ -10,46 +9,3 @@
 
 (def ds
   (jdbc/get-datasource db))
-
-(comment
-  (def my-data
-    #{{:name "sss" :login "abc"}
-      {:name nil :login "bcs"}})
-
-  (jdbc/execute! ds
-                 [create-table-members])
-  ;; => [#:next.jdbc{:update-count 0}]
-
-  (jdbc/execute! ds
-                 [create-table-members-if-not-exists])
-  ;; => [#:next.jdbc{:update-count 0}]
-  ;; => [#:next.jdbc{:update-count 0}]
-
-  (jdbc/execute! ds
-                 [drop-table-members])
-  ;; => [#:next.jdbc{:update-count 0}]
-
-  (jdbc/execute! ds
-                 [drop-table-members-if-exists])
-  ;; => [#:next.jdbc{:update-count 0}]
-  ;; => [#:next.jdbc{:update-count 0}]
-
-  (vec my-data)
-  ;; => [{:name nil, :login "bcs"} {:name "sss", :login "abc"}]
-
-  (vector my-data)
-  ;; => [#{{:name nil, :login "bcs"} {:name "sss", :login "abc"}}]
-
-  (sql/insert! ds :members
-               {:name "someone" :login "ccc"})
-;; => #:MEMBERS{:LOGIN "ccc"}
-
-  (sql/insert-multi! ds :members
-                     [{:login "aaa" :name "sss"}
-                      {:login "uuu" :name "sfaru"}])
-  ;; => [#:MEMBERS{:LOGIN "aaa"} #:MEMBERS{:LOGIN "uuu"}]
-
-  (sql/insert-multi! ds :members
-                     (vec my-data))
-  ;; => [#:MEMBERS{:LOGIN "bcs"} #:MEMBERS{:LOGIN "abc"}]
-  ,)
